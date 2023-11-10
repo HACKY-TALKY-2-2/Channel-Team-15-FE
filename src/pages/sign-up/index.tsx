6931edbb3403c80026800163801e96fa8057909c
littleemm <lilysolar@naver.com>
2023-11-11 05:03:08 +0900

    feat: fix input ui

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -20,14 +20,13 @@ export const SignUp = () => {
                 Email Address
               </label>
             </div>
-            <div className="mt-3">
+            <div className="!mt-3">
               <input
                 id="email"
                 name="email"
                 type="email"
-                autoComplete="current-password"
                 required
-                className="block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="p-3 block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -36,18 +35,18 @@ export const SignUp = () => {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="name"
-                className="block text-xl font-bold leading-6 text-gray-900"
+                className="p-3 block text-xl font-bold leading-6 text-gray-900"
               >
                 Name
               </label>
             </div>
-            <div className="mt-3">
+            <div className="!mt-3">
               <input
                 id="name"
                 name="name"
                 type="name"
                 required
-                className="block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="p-3 block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -56,19 +55,19 @@ export const SignUp = () => {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
-                className="block text-xl font-bold leading-6 text-gray-900"
+                className="p-3 block text-xl font-bold leading-6 text-gray-900"
               >
                 Password
               </label>
             </div>
-            <div className="mt-3">
+            <div className="!mt-3">
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="p-3 block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -82,19 +81,19 @@ export const SignUp = () => {
                 Confirm Password
               </label>
             </div>
-            <div className="mt-3">
+            <div className="!mt-3">
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="p-3 block w-full h-11 rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
 
-          <div className="!my-10">
+          <div className="!my-12">
             <button
               type="submit"
               className="flex my-10 w-full h-10 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
