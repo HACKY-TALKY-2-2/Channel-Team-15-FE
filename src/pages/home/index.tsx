b2a918ca75cb6f4086b710fd438531f720ed1516
김유나 <yuna000804@gmail.com>
2023-11-11 03:40:27 +0900

    fix: main page 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,10 @@
-import {
-  BezierProvider,
-  LightFoundation,
-  Text,
-  Button,
-  Border,
-  Tooltip,
-  Emoji,
-  ListMenuTitle,
-  ProgressBar,
-  Spinner,
-  Banner,
-  Icon,
-} from "@channel.io/bezier-react";
+import { Text, Typography, Icon } from "@channel.io/bezier-react";
 
-import {
-  BulletIcon,
-  ChannelBtnSmileFilledIcon,
-} from "@channel.io/bezier-icons";
+import { ChannelBtnSmileFilledIcon } from "@channel.io/bezier-icons";
 import { useRecoilValue } from "recoil";
 import { experimentState, nicknameState } from "../../recoil/atoms";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import { Tooltip } from "antd";
 
 export const Home = () => {
   const experiment = useRecoilValue(experimentState);
@@ -34,40 +19,46 @@ export const Home = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col">
+    <div className="relative flex flex-col h-screen">
       <div
-        className="flex flex-row items-center"
+        className="flex flex-row items-center rounded-[8px]"
         style={{ backgroundColor: "#5E56F04D" }}
       >
-        <div className="m-6 w-24 h-24 border-2 flex justify-center items-center rounded-full text-3xl">
-          {`${experiment.level}`}
-        </div>
-        <div className="text-4xl">{`${nickname}의 둥둥이`}</div>
+        <button className="flex items-center justify-center w-24 h-16 m-6 text-3xl rounded-full bg-colors-bg-grey-dim-lightest">
+          <Text
+            as="p"
+            typo={Typography.Size18}
+            bold
+          >{`${experiment.level}`}</Text>
+        </button>
+        <Text as="p" typo={Typography.Size24} bold className="p-4">
+          {`${nickname}의 둥둥이`}
+        </Text>
       </div>
-      <div className="flex gap-4 justify-center mt-6">
+      <div className="flex justify-center gap-4 mt-6">
         <div
-          className="w-9 h-9 rounded-full"
+          className="rounded-full w-9 h-9"
           style={{ backgroundColor: "red" }}
           onClick={() => {
             setDoonDoongColor("red");
           }}
         ></div>
         <div
-          className="w-9 h-9 rounded-full"
+          className="rounded-full w-9 h-9"
           style={{ backgroundColor: "blue" }}
           onClick={() => {
             setDoonDoongColor("blue");
           }}
         ></div>
         <div
-          className="w-9 h-9 rounded-full"
+          className="rounded-full w-9 h-9"
           style={{ backgroundColor: "teal" }}
           onClick={() => {
             setDoonDoongColor("teal");
           }}
         ></div>
         <div
-          className="w-9 h-9 rounded-full"
+          className="rounded-full w-9 h-9"
           style={{ backgroundColor: "pink" }}
           onClick={() => {
             setDoonDoongColor("pink");
@@ -75,28 +66,41 @@ export const Home = () => {
         ></div>
       </div>
 
-      <div>{message !== "" ? <div>{message}</div> : null}</div>
+      <div className="absolute flex flex-col items-center justify-center translate-x-1/2 translate-y-1/2 h-1/2">
+        <div className="w-full">
+          <Text as="p" typo={Typography.Size15} bold>
+            {message}
+          </Text>
+        </div>
 
-      <div className="h-1/2 flex justify-center items-center">
-        {doongDoongColor === "red" ? <RedIcon /> : null}
-        {doongDoongColor === "blue" ? <BlueIcon /> : null}
-        {doongDoongColor === "teal" ? <TealIcon /> : null}
-        {doongDoongColor === "pink" ? <GreyIcon /> : null}
+        <div>
+          {doongDoongColor === "red" ? <RedIcon /> : null}
+          {doongDoongColor === "blue" ? <BlueIcon /> : null}
+          {doongDoongColor === "teal" ? <TealIcon /> : null}
+          {doongDoongColor === "pink" ? <GreyIcon /> : null}
+        </div>
       </div>
 
       <div
-        className="flex flex-row items-center justify-around py-8 fixed left-0 bottom-0 w-full"
+        className="rounded-[8px] max-w-[400px] w-full px-[50px] fixed bottom-0 p-4 flex justify-around"
         style={{ backgroundColor: "#5E56F04D" }}
       >
-        <div
-          className=" border-2 w-2/5 h-24 flex items-center justify-center text-3xl rounded-lg"
+        <button
           onClick={talkDoongDoong}
+          className="py-4 px-8 bg-colors-bgtxt-blue-light rounded-[8px]"
         >
-          밥주기
-        </div>
-        <div className=" border-2 w-2/5 h-24 flex items-center justify-center text-3xl rounded-lg">
-          동동이의 추억
-        </div>
+          <Text as="p" typo={Typography.Size15} bold>
+            밥주기
+          </Text>
+        </button>
+        <button
+          onClick={() => {}}
+          className="py-4 px-8 bg-colors-bgtxt-blue-light rounded-[8px]"
+        >
+          <Text as="p" typo={Typography.Size15} bold>
+            동동이의 추억
+          </Text>
+        </button>
       </div>
     </div>
   );
