6867412cae93c3850edc9bca81471e29f18c97e7
김유나 <yuna000804@gmail.com>
2023-11-10 23:49:25 +0900

    feat: width 400px 고정, custom color theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("./src/theme/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 };
