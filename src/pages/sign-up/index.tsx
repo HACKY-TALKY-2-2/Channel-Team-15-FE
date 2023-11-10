14ed7f217317d1258abdd213a8158b5d0eb7bd86
littleemm <lilysolar@naver.com>
2023-11-11 03:53:20 +0900

    feat: fix form design

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,3 +1,4 @@
+import { FormControl, FormLabel, TextField } from "@channel.io/bezier-react";
 import React from "react";
 
 export const SignUp = () => {
@@ -12,62 +13,36 @@ export const SignUp = () => {
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST">
           <div className="my-10">
-            <label
-              htmlFor="email"
-              className="block text-xl font-bold leading-6 text-gray-900"
+            <FormControl
+              id="email"
+              labelPosition="top"
+              leftLabelWrapperHeight={54}
             >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+              <FormLabel>Email Address</FormLabel>
+              <TextField />
+            </FormControl>
           </div>
 
-          <div className="!my-10">
-            <label
-              htmlFor="name"
-              className="block text-xl font-bold leading-6 text-gray-900"
+          <div className="my-10">
+            <FormControl
+              id="name"
+              labelPosition="top"
+              leftLabelWrapperHeight={54}
             >
-              Name
-            </label>
-            <div className="mt-2">
-              <input
-                id="name"
-                name="name"
-                type="name"
-                autoComplete="name"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+              <FormLabel>Name</FormLabel>
+              <TextField />
+            </FormControl>
           </div>
 
-          <div className="!my-10">
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-xl font-bold leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+          <div className="my-10">
+            <FormControl
+              id="password"
+              labelPosition="top"
+              leftLabelWrapperHeight={54}
+            >
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <TextField />
+            </FormControl>
           </div>
 
           <div className="!my-10">
